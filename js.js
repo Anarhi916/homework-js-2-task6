a76e06cb908c6f13274382f5c728a4d437c9ea4c
Andrey Filimonov <gipget@mail.ru>
2017-07-15 10:04:24 +0300

    c

--- js.js
+++ js.js
@@ -2,23 +2,25 @@ let task1 = document.querySelector('.task1');
 let task2 = document.querySelector('.task2');
 let task3 = document.querySelector('.task3');
 
-task1.textContent = 'Задание 1: ';
-task2.textContent = 'Задание 2: ';
-task3.textContent = 'Задание 3: ';
+let numbers1 = [];
+let numbers2 = [];
+let numbers3 = 0;
 
-for (var i = 0; i < 102; i++) {
+for (let i = 0; i < 102; i++) {
     if(i % 2 == 0){
-        task1.textContent += i + ' ';
+        numbers1 += i + ' ';
     }    
 }
+task1.textContent = 'Задание 1: ' + numbers1;
 
-for (var i = 200; i >= 0; i--) {    
-        task2.textContent += i + ' ';        
+for (let i = 200; i >= 0; i--) {    
+        numbers2 += i + ' ';        
+}
+task2.textContent = 'Задание 2: ' + numbers2;
+
+
+for (let i = 0; i < 101; i++) {    
+       numbers3 +=i;         
 }
 
-let number = 0;
-for (var i = 0; i < 101; i++) {    
-       number +=i;         
-}
-
-task3.textContent += number;
+task3.textContent = 'Задание 3: ' + numbers3;
